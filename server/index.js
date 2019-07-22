c3906d823956cc0f422f25bade9c7c49a5d0697a
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-07-22 12:02:38 +0800

    req.params done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const getProducts = require('./getProducts');
 const PORT = 3000;
 
 app.get('/api/products', getProducts);
-
+app.get('/api/products/:id', getProducts);
 app.listen(PORT, (err) => {
 	if (err) return console.error(err);
 	console.log(`Server is running on PORT: ${PORT}`);
