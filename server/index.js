63529bda6665046dd26baad911e012f1fe84bca8
MikeMikael12 <jmbolima12@gmail.com>
2019-07-22 16:15:16 +0800

    Node-1: complete

---
+++ server/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const getProducts = require('./getProducts.js');
+
+const getProduct = require('./getProduct.js');
+
+const app = express();
+
+const port = 3002;
+
+
+app.get('/api/products', getProducts);
+app.get('/api/product/:id', getProduct);
+
+app.listen(port, () => { console.log(`Server listening on port: ${port}`) });
