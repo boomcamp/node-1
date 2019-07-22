0b7463f106e9a82aea0ebe6b36cb206b28dfd10a
Sidney Bercasio <bercasio.sidney@gmail.com>
2019-07-22 14:10:14 +0800

    done filter

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,6 +1,10 @@
 const products = require('../products.json');
 
 const getProducts = (req, res) => {
+	if (req.query.price) {
+		const item = products.filter((prod) => prod.price >= parseInt(req.query.price));
+		res.status(200).send(item);
+	}
 	res.status(200).send(products);
 };
 
