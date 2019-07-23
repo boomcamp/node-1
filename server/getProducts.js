c79d758c022eb065c3936622b4cf6b3cdc2f03eb
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-07-23 10:08:49 +0800

    node-1 done, show err !price>query

--- server/getProducts.js
+++ server/getProducts.js
@@ -4,7 +4,7 @@ const getProducts = (req, res) => {
 	
 	if (req.query.price){
 		const item = products.filter(val => val.price >= parseInt(req.query.price))
-		if (!item){
+		if (item.length === 0){
 			return res.status(500).send('no items found');
 		}
 		return res.status(200).send(item)
