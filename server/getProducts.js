5231c39605e561fce221e96f26b2721b4204f5fe
rapraquion <rapraquion@gmail.com>
2019-12-30 14:04:20 -0500

    Step 6 done

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,11 +1,12 @@
 const products = require('../products.json')
 
 const getProducts = (req, res) => {
-    const prod = products.find(data => data.id === Number(req.params.id));
-    if (!prod) {
-        return res.status(500).send('Item not on the list');
+    if (req.query.price) {
+        const item = products.filter(val => val.price >= Number(req.query.price))
+        return res.status(200).send(item)
     }
-    res.status(200).send(prod);
+    res.status(200).send(products);
+
 }
 
 module.exports = getProducts
