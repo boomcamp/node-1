1d8b3ed61661737aa592a3338466706a74042a19
rapraquion <rapraquion@gmail.com>
2019-12-30 11:39:13 -0500

    Step 3 done

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-// const products = require('../products.json');
+const products = require('../products.json');
 
 const app = express();
 
 const PORT = 3000;
+
+app.get('/api/products', (req, res) => {
+    res.status(200).send(products);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
