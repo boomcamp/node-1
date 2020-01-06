1332af7b254803ce991560d7608d91bd71fb2b6b
franco <francisco.ifurung@boom.camp>
2020-01-06 16:00:33 +0800

    final commit

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,29 +1,13 @@
-// function add(a,b){
-//     return a + b;
-// }
+const products = require('../products.json');
 
-// function mul(a,b){
-//     return a * b;
-// }
-
-// function selReverse(arr, id){
-//     var result = []
-//     if(id !== 0){
-//       arr.map((x) => result.push(...arr.splice(0, id).reverse()))
-//       result.push(...arr.reverse())
-//     }else{
-//       return arr
-//     }
-//     return result
-// }
-
-// module.exports = {
-//     add, mul, selReverse
-// }
-// console.log(add(2,5))
-
-module.exports = {
-    add: function(arr){
-      return arr.map(x => x*2)
-    }
+const getProducts = (req, res) => {
+  if (req.query.price) {
+    const items = products.filter(
+      val => val.price >= parseInt(req.query.price)
+    );
+    return res.status(200).send(items);
   }
+  res.status(200).send(products);
+};
+
+module.exports = getProducts;
--- server/index.js
+++ server/index.js
@@ -1,35 +1,14 @@
-const path = require('path')
-const express = require('express')
-const http = require('http')
+const express = require('express');
 const getProducts = require('./getProducts');
+const getProduct = require('./getProduct');
 
-const app = express()
-// const { add, mul, selReverse } = require('./add.js')
+const app = express();
 
-// const {add} = require('./add.js')
-// console.log(add([1,2,3]))
-// const num1 = add(2,3)
-// const num2 = mul(2,3)
+const port = 3001;
 
-// const product = num1 * num2
-// console.log(product, selReverse([2,4,6,8,10,12,14,16], 3))
-// const server = http.createServer((req, res) => {
-//     console.log(req.url)
+app.get('/api/products', getProducts);
+app.get('/api/product/:id', getProduct);
 
-//     res.end("Hello World!");
-// })
-const port = 3000
-// server.listen(port, (err)=>{
-//     if(err){
-//         console.log('error')
-//     }
-//     console.log(`Listening to Port ${port}`)
-// })
-app.get('/api/products', getProducts)
-app.listen(port, (err) =>{
-    if(err) {
-        return console.log(error)
-    }
-
-    console.log(`Listening to Port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
